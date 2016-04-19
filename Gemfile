0698bf45845ef0707a8f4c8884ff009f2c8c0103
roksana1 <roksanawantulok@gmail.com>
2016-04-19 14:19:37 +0000

    obrazy

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 source 'https://rubygems.org'
 
+gem 'paperclip', '~> 4.3', '>= 4.3.6'
 gem 'rails', '4.2.5'
 gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -59,7 +59,7 @@ class PinsController < ApplicationController
       format.json { head :no_content }
     end
   end
-
+  
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_pin
@@ -68,7 +68,7 @@ class PinsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def pin_params
-      params.require(:pin).permit(:description)
+      params.require(:pin).permit(:description, :image)
     end
     def correct_user
       @pin = current_user.pins.find_by(id: params[:id])
